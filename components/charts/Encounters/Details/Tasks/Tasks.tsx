ff7ebebc42a484a4d524c6711e3f903cbb61c6b4
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-03 16:34:24 +0530

    Update Tasks

--- components/charts/Encounters/Details/Tasks/Tasks.tsx
+++ components/charts/Encounters/Details/Tasks/Tasks.tsx
@@ -7,15 +7,20 @@ import {
 import React from "react";
 import TasksDialog from "./TasksDialog";
 import TasksList from "./TasksList";
+import { UserEncounterData } from "@/types/chartsInterface";
 
-const Tasks = () => {
+const Tasks = ({
+  patientDetails,
+}: {
+  patientDetails: UserEncounterData;
+}) => {
   return (
     <div className="flex flex-col gap-3">
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="tasks">
           <div className="flex justify-between items-center">
             <AccordionTrigger>Tasks</AccordionTrigger>
-            <TasksDialog />
+            <TasksDialog patientDetails={patientDetails}/>
           </div>
           <AccordionContent className="sm:max-w-4xl">
             <TasksList />
