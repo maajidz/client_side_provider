603ddfe74189d86e3250026e4f43c8aaf3077821
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-10 12:11:45 +0530

    Remove FlowSheet,TreatmentPlan, &  Questionnaires from Preview Body

--- components/charts/Encounters/Preview/PreviewBody.tsx
+++ components/charts/Encounters/Preview/PreviewBody.tsx
@@ -5,14 +5,14 @@ import {
     TabsList,
     TabsTrigger,
 } from "@/components/ui/tabs"
-import { Clipboard, ClipboardPlus, FileQuestion, FileText, FlaskConical, Target } from 'lucide-react';
+import { Clipboard, ClipboardPlus, FileText, FlaskConical } from 'lucide-react';
 import ChartNotes from './ChartNotes';
-import FlowSheet from './FlowSheet';
-import TreatmentPlan from './TreatmentPlan';
+// import FlowSheet from './FlowSheet';
+// import TreatmentPlan from './TreatmentPlan';
 import LabsPreview from './LabsPreview';
 import ImagesPreview from './ImagesPreview';
 import DocumentPreview from './DocumentPreview';
-import QuestionnairePreview from './QuestionnairePreview';
+// import QuestionnairePreview from './QuestionnairePreview';
 import { UserEncounterData } from '@/types/chartsInterface';
 
 const PreviewBody = ({ patientDetails }: { patientDetails: UserEncounterData }) => {
@@ -21,22 +21,22 @@ const PreviewBody = ({ patientDetails }: { patientDetails: UserEncounterData }) 
             <Tabs defaultValue="chartNotes" className="w-full">
                 <TabsList className="flex w-full justify-between">
                     <TabsTrigger value="chartNotes"><Clipboard /></TabsTrigger>
-                    <TabsTrigger value="flowsheet"><Target /></TabsTrigger>
-                    <TabsTrigger value="treatmentPlan"><Target /></TabsTrigger>
+                    {/* <TabsTrigger value="flowsheet"><Target /></TabsTrigger>
+                    <TabsTrigger value="treatmentPlan"><Target /></TabsTrigger> */}
                     <TabsTrigger value="labs"><FlaskConical /></TabsTrigger>
                     <TabsTrigger value="images"><ClipboardPlus /></TabsTrigger>
                     <TabsTrigger value="documents"><FileText /></TabsTrigger>
-                    <TabsTrigger value="questionnaires"><FileQuestion /></TabsTrigger>
+                    {/* <TabsTrigger value="questionnaires"><FileQuestion /></TabsTrigger> */}
                 </TabsList>
                 <TabsContent value="chartNotes" className='md:px-0 p-0'>
                     <ChartNotes patientDetails={patientDetails}/>
                 </TabsContent>
-                <TabsContent value="flowsheet">
+                {/* <TabsContent value="flowsheet">
                     <FlowSheet />
                 </TabsContent>
                 <TabsContent value="treatmentPlan">
                     <TreatmentPlan />
-                </TabsContent>
+                </TabsContent> */}
                 <TabsContent value="labs">
                     <LabsPreview />
                 </TabsContent>
@@ -46,9 +46,9 @@ const PreviewBody = ({ patientDetails }: { patientDetails: UserEncounterData }) 
                 <TabsContent value="documents">
                     <DocumentPreview />
                 </TabsContent>
-                <TabsContent value="questionnaires">
+                {/* <TabsContent value="questionnaires">
                     <QuestionnairePreview />
-                </TabsContent>
+                </TabsContent> */}
             </Tabs>
         </div>
     )
