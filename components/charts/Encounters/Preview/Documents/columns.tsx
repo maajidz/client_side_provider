05aadbf04c70f72eb221172dc7fcdd63a3ab69e1
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-10 15:25:37 +0530

    Fix accessor key

--- components/charts/Encounters/Preview/Documents/columns.tsx
+++ components/charts/Encounters/Preview/Documents/columns.tsx
@@ -1,25 +1,9 @@
-"use client";
-import { Button } from "@/components/ui/button";
+"use client"
 import { ImageResultDataResponse } from "@/types/imageResults";
 import { ColumnDef } from "@tanstack/react-table";
+import Link from "next/link";
 
 export const columns = (): ColumnDef<ImageResultDataResponse>[] => [
-  {
-    accessorKey: "testResults",
-    header: "Image Type",
-    cell: ({ row }) => {
-      const testResults = row.getValue(
-        "testResults"
-      ) as ImageResultDataResponse["testResults"];
-      return (
-        <div className="cursor-pointer">
-          {testResults.map((results) => 
-              results.imageTest.imageType.name
-          )}
-        </div>
-      );
-    },
-  },
   {
     accessorKey: "testResults",
     header: "Documents",
@@ -29,15 +13,23 @@ export const columns = (): ColumnDef<ImageResultDataResponse>[] => [
       ) as ImageResultDataResponse["testResults"];
       return (
         <div className="cursor-pointer">
-          {testResults.map((results) => (
-            <div key={results.id} className="flex flex-col gap-2">
-                  {results.documents?.map((docs, index) => (
-                    <Button key={index} variant={"link"} onClick={()=> window.open(docs, "_blank")} className="text-blue-500">{docs.split('/').pop()}</Button>
-                  ))}
-            </div>
-          ))}
+          {testResults.flatMap((results) =>
+            results.documents?.map((docs, index) => (
+              // <Button
+              //   key={index}
+              //   variant={"link"}
+              //   onClick={() => window.open(docs, "_blank")}
+              //   className="text-blue-500"
+              // >
+              //   {docs.split("/").pop()}
+              // </Button>
+              <Link key={index} href={docs} className="text-blue-500">
+                 {docs.split("/").pop()}
+              </Link>
+            ))
+          )}
         </div>
       );
     },
-  }
+  },
 ];
