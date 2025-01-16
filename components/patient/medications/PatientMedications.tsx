dc2420be2ebac1cf3e1efad1c4b92926d83a711a
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-16 17:59:56 +0530

    Patient Medications

--- components/patient/medications/PatientMedications.tsx
+++ components/patient/medications/PatientMedications.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { PatientMedicationsClient } from "@/components/tables/patient/patientMedications/client";
+import React from "react";
 
-const PatientMedications = ({userDetailsId}: {userDetailsId: string}) => {
+const PatientMedications = ({ userDetailsId }: { userDetailsId: string }) => {
   return (
-    <div>PatientMedications {userDetailsId}</div>
-  )
-}
+    <>
+      <div>PatientMedications {userDetailsId}</div>
+      <PatientMedicationsClient userDetailsId={userDetailsId} />
+    </>
+  );
+};
 
-export default PatientMedications
+export default PatientMedications;
