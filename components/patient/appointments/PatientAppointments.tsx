095d95da6f39f096bc3b67e16746657f4da18535
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-16 17:59:41 +0530

    Patient Appointments

--- components/patient/appointments/PatientAppointments.tsx
+++ components/patient/appointments/PatientAppointments.tsx
@@ -1,9 +1,11 @@
+import { PatientAppointmentClient } from '@/components/tables/patient/patientAppointments/client'
 import React from 'react'
 
 const PatientAppointments = ({userDetailsId}: {userDetailsId: string}) => {
   return (
     <>
     PATIENT APPOINTMENTS {userDetailsId}
+    <PatientAppointmentClient userDetailsId={userDetailsId}/>
     </>
   )
 }
