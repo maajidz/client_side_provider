8abb2ffcb0439c660943a62b91fbafbc457a6399
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-17 11:45:38 +0530

    Patient Allergies

--- components/patient/allergies/PatientAllergies.tsx
+++ components/patient/allergies/PatientAllergies.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+import AllergiesDialog from "@/components/charts/Encounters/Details/Allergies/AllergiesDialog";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import React, { useState } from "react";
+import ViewPatientAllergies from "./ViewPatientAllergies";
 
 const PatientAllergies = ({ userDetailsId }: { userDetailsId: string }) => {
-  return <div>PatientAllergies {userDetailsId}</div>;
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+  
+  return (
+    <>
+      <div className="flex justify-end">
+        <Button
+          className="bg-[#84012A]"
+          onClick={() => {
+            setIsDialogOpen(true);
+          }}
+        >
+          <div className="flex gap-2">
+            <PlusIcon />
+            Allergy
+          </div>
+        </Button>
+        <AllergiesDialog
+          userDetailsId={userDetailsId}
+          onClose={() => {
+            setIsDialogOpen(false);
+          }}
+          isOpen={isDialogOpen}
+        />
+      </div>
+      <ViewPatientAllergies userDetailsId={userDetailsId} />
+    </>
+  );
 };
 
 export default PatientAllergies;
---
+++ components/patient/allergies/ViewPatientAllergies.tsx
@@ -0,0 +1,89 @@
+import React, { useCallback, useEffect, useState } from "react";
+import { DataTable } from "@/components/ui/data-table";
+import { columns } from "./columns";
+import LoadingButton from "@/components/LoadingButton";
+import { getAllergiesData } from "@/services/chartDetailsServices";
+import { showToast } from "@/utils/utils";
+import { useToast } from "@/components/ui/use-toast";
+import { AllergenResponseInterfae } from "@/types/allergyInterface";
+import EditAllergy from "@/components/charts/Encounters/Details/Allergies/EditAllergy";
+
+const ViewPatientAllergies = ({ userDetailsId }: { userDetailsId: string }) => {
+  const [page, setPage] = useState<number>(1);
+  const limit = 8;
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const { toast } = useToast();
+
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<AllergenResponseInterfae[]>([]);
+  const [editData, setEditData] = useState<AllergenResponseInterfae>();
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+  const fetchAllergies = useCallback(
+    async (page: number) => {
+      // setLoading(true);
+      try {
+        const response = await getAllergiesData({
+          limit: limit,
+          page: page,
+          userDetailsId: userDetailsId,
+        });
+        if (response) {
+          setData(response);
+          setTotalPages(response.length / limit);
+        }
+      } catch (e) {
+        console.log("Error", e);
+      } finally {
+        setLoading(false);
+      }
+    },
+    [userDetailsId]
+  );
+
+  useEffect(() => {
+    fetchAllergies(page);
+  }, [fetchAllergies, page]);
+
+  if (loading) {
+    return <LoadingButton />;
+  }
+
+  return (
+    <>
+      <div className="py-5">
+        {data && (
+          <DataTable
+            searchKey="id"
+            columns={columns({
+              setEditData,
+              setIsDialogOpen,
+              setLoading,
+              showToast: () =>
+                showToast({
+                  toast,
+                  type: "success",
+                  message: "Deleted Successfully",
+                }),
+              fetchAllergies: () => fetchAllergies(page),
+            })}
+            data={data}
+            pageNo={page}
+            totalPages={totalPages}
+            onPageChange={(newPage: number) => setPage(newPage)}
+          />
+        )}
+      </div>
+        <EditAllergy
+          selectedAllergy={editData}
+          onClose={() => {
+            setIsDialogOpen(false);
+          }}
+          isOpen={isDialogOpen}
+          fetchAllergies={() => fetchAllergies(page)}
+        />
+    </>
+  );
+};
+
+export default ViewPatientAllergies;
