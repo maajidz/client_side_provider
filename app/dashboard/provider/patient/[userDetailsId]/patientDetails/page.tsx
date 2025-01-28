31483ca6fc842bff35c4de73625cae2b2968f863
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-28 18:54:45 +0530

    Remove breadcrumbs from patient page

--- app/dashboard/provider/patient/[userDetailsId]/patientDetails/page.tsx
+++ app/dashboard/provider/patient/[userDetailsId]/patientDetails/page.tsx
@@ -1,8 +1,6 @@
 "use client";
-import { Breadcrumbs } from "@/components/breadcrumbs";
 import PageContainer from "@/components/layout/page-container";
 import PatientDetails from "@/components/patient/patientDetails/PatientDetails";
-import { Heading } from "@/components/ui/heading";
 import { useParams } from "next/navigation";
 
 function Page() {
@@ -11,22 +9,22 @@ function Page() {
   if (!userDetailsId || Array.isArray(userDetailsId)) {
     return <div>Error: User details ID not found</div>;
   }
-  
-  const breadcrumbItems = [
-    { title: "Dashboard", link: "/dashboard" },
-    { title: "Patients", link: "/dashboard/provider/patient" },
-    {
-      title: "Patient Details",
-      link: `/dashboard/provider/patient/${userDetailsId}/patientDetails`,
-    },
-  ];
+
+  // const breadcrumbItems = [
+  //   { title: "Dashboard", link: "/dashboard" },
+  //   { title: "Patients", link: "/dashboard/provider/patient" },
+  //   {
+  //     title: "Patient Details",
+  //     link: `/dashboard/provider/patient/${userDetailsId}/patientDetails`,
+  //   },
+  // ];
 
   return (
     <PageContainer scrollable={true}>
-      <div className="space-y-4">
+      {/* <div className="space-y-4">
         <Breadcrumbs items={breadcrumbItems} />
         <Heading title="Patient Details" description="" />
-      </div>
+      </div> */}
       <PatientDetails userId={userDetailsId} />
     </PageContainer>
   );
