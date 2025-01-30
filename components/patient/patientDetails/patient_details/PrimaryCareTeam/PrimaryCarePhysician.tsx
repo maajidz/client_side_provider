51d1096880e3d2acd6fef6632e17b18644ed4826
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-30 14:58:32 +0530

    Patient Care Team

--- components/patient/patientDetails/patient_details/PrimaryCareTeam/PrimaryCarePhysician.tsx
+++ components/patient/patientDetails/patient_details/PrimaryCareTeam/PrimaryCarePhysician.tsx
@@ -6,7 +6,11 @@ import { searchProviders } from "@/services/registerServices";
 import LoadingButton from "@/components/LoadingButton";
 import { PatientCareTeamInterface } from "@/types/userInterface";
 
-const PrimaryCarePhysician = ({careTeam}: {careTeam: PatientCareTeamInterface | null}) => {
+const PrimaryCarePhysician = ({
+  careTeam,
+}: {
+  careTeam: PatientCareTeamInterface | null;
+}) => {
   const [providers, setProviders] = useState<FetchProviderList[]>([]);
   const [searchPrimaryCarePhysician, setSearchPrimaryCarePhysician] =
     useState("");
@@ -59,47 +63,64 @@ const PrimaryCarePhysician = ({careTeam}: {careTeam: PatientCareTeamInterface | 
   return (
     <div className={styles.infoBox}>
       <div className={styles.infoBoxLabel}>Primary Care Physician</div>
+      {/* {selectedPrimaryCarePhysician ? <div>Edit </div> :  */}
       <div>
-        <Input
-          value={searchPrimaryCarePhysician}
-          onChange={(e) => {
-            setSearchPrimaryCarePhysician(e.target.value);
-            setSelectedPrimaryCarePhysician(null);
-          }}
-          placeholder="Search providers..."
-        />
-      </div>
-      <div className="relative w-full">
-        {loading && (
-          <p className="absolute bg-white p-2 rounded shadow-lg">Loading...</p>
-        )}
-        {!selectedPrimaryCarePhysician && providers.length > 0 ? (
-          <div className="absolute mt-2 w-full bg-white shadow-lg rounded-lg z-10">
-            {providers.map((user) => (
-              <div
-                key={user.id}
-                className="border-2 border-gray-300 rounded-lg p-2 hover:bg-gray-100 cursor-pointer"
-                onClick={() => handlePrimaryCarePhysicianSelect(user)}
-              >
-                <div className="text-[#84012A] text-base font-medium">
-                  {user.firstName} {user.lastName}
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          !selectedPrimaryCarePhysician &&
-          !loading &&
-          searchPrimaryCarePhysician && (
+        <div>
+          <Input
+            value={searchPrimaryCarePhysician}
+            onChange={(e) => {
+              setSearchPrimaryCarePhysician(e.target.value);
+              setSelectedPrimaryCarePhysician(null);
+            }}
+            placeholder="Search providers..."
+          />
+        </div>
+        <div className="relative w-full">
+          {loading && (
             <p className="absolute bg-white p-2 rounded shadow-lg">
-              No results found.
+              Loading...
             </p>
-          )
-        )}
+          )}
+          {!selectedPrimaryCarePhysician && providers.length > 0 ? (
+            <div className="absolute mt-2 w-full bg-white shadow-lg rounded-lg z-10">
+              {providers.map((user) => (
+                <div
+                  key={user.id}
+                  className="border-2 border-gray-300 rounded-lg p-2 hover:bg-gray-100 cursor-pointer"
+                  onClick={() => handlePrimaryCarePhysicianSelect(user)}
+                >
+                  <div className="text-[#84012A] text-base font-medium">
+                    {user.firstName} {user.lastName}
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) : (
+            !selectedPrimaryCarePhysician &&
+            !loading &&
+            searchPrimaryCarePhysician && (
+              <p className="absolute bg-white p-2 rounded shadow-lg">
+                No results found.
+              </p>
+            )
+          )}
+        </div>
       </div>
-      <div>{careTeam?.primaryCarePhysician.NameOfPhysician}</div>
+      {/* } */}
+      <div className={styles.physicianDetailsBox}>
+        <PhysicianData label="Name of Physician" value={careTeam?.primaryCarePhysician.NameOfPhysician || ''} />
+      </div>
     </div>
   );
 };
 
 export default PrimaryCarePhysician;
+
+const PhysicianData = ({ label, value }: { label: string; value: string }) => {
+  return (
+    <div className="">
+      <div className={styles.labelText}>{label}</div>
+      <div className={styles.valueText}>{value}</div>
+    </div>
+  );
+};
