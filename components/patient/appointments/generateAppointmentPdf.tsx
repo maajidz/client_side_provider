54584df728eedabce8e7c27c6bef37546b4ddb52
ahbazarkoob <ahbazarkoob@gmail.com>
2025-01-31 17:48:00 +0530

    Patient Appointment

---
+++ components/patient/appointments/generateAppointmentPdf.tsx
@@ -0,0 +1,46 @@
+import jsPDF from "jspdf";
+import autoTable from "jspdf-autotable";
+import { UserAppointmentInterface } from "@/types/userInterface";
+
+const generateAppointmentPDF = ({
+  appointmentData,
+}: {
+  appointmentData: UserAppointmentInterface[];
+}) => {
+  const doc = new jsPDF();
+  const pageWidth = doc.internal.pageSize.width;
+
+  // Header Section
+  doc.setFontSize(12);
+  doc.text("Pomegranate Health", pageWidth - 10, 10, { align: "right" });
+  doc.text("30 N Gould Street", pageWidth - 10, 15, { align: "right" });
+  doc.text("Ste R, Sheridan, Wyoming - 82801", pageWidth - 10, 20, {
+    align: "right",
+  });
+
+  doc.line(10, 25, pageWidth - 10, 25);
+
+  doc.setFontSize(14);
+  doc.text("Patient Tasks", 10, 35);
+
+  autoTable(doc, {
+    startY: 40,
+    head: [["Task ID", "Category", "Status", "Priority"]],
+    body: appointmentData.map((appointment) => [
+      appointment.id,
+      appointment.patientName,
+      appointment.patientEmail,
+      appointment.patientPhoneNumber,
+      appointment.providerId,
+      appointment.providerName,
+      appointment.dateOfAppointment,
+      `${appointment.timeOfAppointment}- ${appointment.endtimeOfAppointment}`,
+      appointment.additionalText,
+      appointment.meetingLink
+    ]),
+  });
+
+  doc.save("Patient_Tasks.pdf");
+};
+
+export default generateAppointmentPDF;
