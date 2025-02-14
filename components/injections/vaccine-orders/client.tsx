d3e0501b4d86d9f4f679038038098e7555c633a5
ahbazarkoob <ahbazarkoob@gmail.com>
2025-02-14 11:58:56 +0530

    Fix status filter for Vacccine Orders

--- components/injections/vaccine-orders/client.tsx
+++ components/injections/vaccine-orders/client.tsx
@@ -2,17 +2,44 @@ import LoadingButton from "@/components/LoadingButton";
 import { getVaccinesData } from "@/services/injectionsServices";
 import { VaccinesInterface } from "@/types/injectionsInterface";
 import { columns } from "./column";
-import FilterVaccines from "./FilterVaccines";
+// import FilterVaccines from "./FilterVaccines";
 import { useCallback, useEffect, useState } from "react";
 import { vaccineSearchParams } from "@/schema/injectionsAndVaccinesSchema";
 import { z } from "zod";
 import { DefaultDataTable } from "@/components/custom_buttons/table/DefaultDataTable";
 import { showToast } from "@/utils/utils";
 import { useToast } from "@/hooks/use-toast";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import SubmitButton from "@/components/custom_buttons/buttons/SubmitButton";
+import { UserData } from "@/types/userInterface";
+import { FetchProviderList } from "@/types/providerDetailsInterface";
+import { fetchUserDataResponse } from "@/services/userServices";
+import { fetchProviderListDetails } from "@/services/registerServices";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 function VaccinesClient({ refreshTrigger }: { refreshTrigger: number }) {
   // Data State
   const [vaccinesData, setVaccinesData] = useState<VaccinesInterface[]>([]);
+  const [patientData, setPatientData] = useState<UserData[]>([]);
+  const [providersList, setProvidersList] = useState<FetchProviderList[]>([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+  const [visibleSearchList, setVisibleSearchList] = useState<boolean>(false);
 
   // Pagination State
   const itemsPerPage = 10;
@@ -29,6 +56,68 @@ function VaccinesClient({ refreshTrigger }: { refreshTrigger: number }) {
 
   // Loading State
   const [loading, setLoading] = useState(false);
+
+  const fetchUserData = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetchUserDataResponse({
+        pageNo: 1,
+        pageSize: 10,
+        firstName: searchTerm,
+        lastName: searchTerm,
+      });
+
+      if (response) {
+        setPatientData(response.data);
+      } else {
+        throw new Error();
+      }
+    } catch (err) {
+      showToast({
+        toast,
+        type: "error",
+        message: "Could not fetch patients",
+      });
+      console.log("Error:", err);
+    } finally {
+      setLoading(false);
+    }
+  }, [searchTerm, toast]);
+
+  const fetchProvidersData = useCallback(async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetchProviderListDetails({ page: 1, limit: 10 });
+
+      setProvidersList(response.data);
+    } catch (err) {
+      showToast({
+        toast,
+        type: "error",
+        message: "Could not fetch providers",
+      });
+      console.log("Error:", err);
+    } finally {
+      setLoading(false);
+    }
+  }, [toast]);
+
+  useEffect(() => {
+    fetchUserData();
+    fetchProvidersData();
+  }, [fetchUserData, fetchProvidersData]);
+
+  const filteredPatients = patientData.filter((patient) =>
+    `${patient.user.firstName} ${patient.user.lastName}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
+
+  const form = useForm<z.infer<typeof vaccineSearchParams>>({
+    resolver: zodResolver(vaccineSearchParams),
+  });
 
   // GET Injections Data
   const fetchInjectionsData = useCallback(async () => {
@@ -54,22 +143,18 @@ function VaccinesClient({ refreshTrigger }: { refreshTrigger: number }) {
     }
   }, [pageNo, filters.userDetailsId, filters.providerId, filters.status]);
 
-  const handleSearch = (filterValues: z.infer<typeof vaccineSearchParams>) => {
-    if (filterValues.userDetailsId === "all") {
-      filterValues.status = "";
-    }
-
-    if (filterValues.providerId === "all") {
-      filterValues.providerId = "";
-    }
-
+  function onSubmit(values: z.infer<typeof vaccineSearchParams>) {
     setFilters({
-      providerId: filterValues.providerId || "",
-      userDetailsId: filterValues.userDetailsId || "",
-      status: filterValues.status || "",
+      providerId:
+        values.providerId === "all" ? "" : values.providerId || "",
+      userDetailsId:
+        values.userDetailsId === "all"
+          ? ""
+          : values.userDetailsId || "",
+      status: values.status === "all" ? "" : values.status || "",
     });
     setPageNo(1);
-  };
+  }
 
   // Effects
   useEffect(() => {
@@ -81,10 +166,151 @@ function VaccinesClient({ refreshTrigger }: { refreshTrigger: number }) {
   return (
     <div className="space-y-2">
       <div className="space-y-2">
-        <FilterVaccines
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4"
+          >
+            {/* Ordered By Filter */}
+            <FormField
+              control={form.control}
+              name="providerId"
+              render={({ field }) => (
+                <FormItem className="flex items-center">
+                  <FormControl>
+                    <Select value={field.value} onValueChange={field.onChange}>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Filter by Ordered by" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="all" className="cursor-pointer">
+                          All
+                        </SelectItem>
+                        {providersList
+                          .filter(
+                            (
+                              provider
+                            ): provider is typeof provider & {
+                              providerDetails: { id: string };
+                            } => Boolean(provider?.providerDetails?.id)
+                          )
+                          .map((provider) => (
+                            <SelectItem
+                              key={provider.id}
+                              value={provider.providerDetails.id}
+                              className="cursor-pointer"
+                            >
+                              {provider.firstName} {provider.lastName}
+                            </SelectItem>
+                          ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage className="text-red-500" />
+                </FormItem>
+              )}
+            />
+
+            {/* Status Filter */}
+            <FormField
+              control={form.control}
+              name="status"
+              render={({ field }) => (
+                <FormItem className="flex items-center">
+                  <FormControl>
+                    <Select
+                      defaultValue={field.value}
+                      onValueChange={field.onChange}
+                    >
+                      <SelectTrigger>
+                        <SelectValue placeholder="Filter by Status" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="all" className="cursor-pointer">
+                          All
+                        </SelectItem>
+                        {Array.from(
+                          new Set(
+                            vaccinesData?.map((vaccine) => vaccine?.status)
+                          )
+                        ).map((status) => (
+                          <SelectItem
+                            key={status}
+                            value={status}
+                            className="cursor-pointer"
+                          >
+                            {status}
+                          </SelectItem>
+                        ))}
+                      </SelectContent>
+                    </Select>
+                  </FormControl>
+                  <FormMessage className="text-red-500" />
+                </FormItem>
+              )}
+            />
+
+            {/* Patient Filter */}
+            <FormField
+              control={form.control}
+              name="userDetailsId"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <div className="relative">
+                      <Input
+                        placeholder="Search Patient "
+                        value={searchTerm}
+                        onChange={(e) => {
+                          const value = e.target.value;
+                          setSearchTerm(value);
+                          setVisibleSearchList(true);
+
+                          if (!value) {
+                            field.onChange("");
+                          }
+                        }}
+                      />
+                      {searchTerm && visibleSearchList && (
+                        <div className="absolute bg-white border border-gray-300 mt-1 rounded shadow-lg  w-full">
+                          {filteredPatients.length > 0 ? (
+                            filteredPatients.map((patient) => (
+                              <div
+                                key={patient.id}
+                                className="px-4 py-2 cursor-pointer hover:bg-gray-100"
+                                onClick={() => {
+                                  field.onChange(patient.id);
+                                  setSearchTerm(
+                                    `${patient.user.firstName} ${patient.user.lastName}`
+                                  );
+                                  setVisibleSearchList(false);
+                                }}
+                              >
+                                {`${patient.user.firstName} ${patient.user.lastName}`}
+                              </div>
+                            ))
+                          ) : (
+                            <div className="px-4 py-2 text-gray-500">
+                              No results found
+                            </div>
+                          )}
+                        </div>
+                      )}
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-end">
+              <SubmitButton label="Search" />
+            </div>
+          </form>
+        </Form>
+        {/* <FilterVaccines
           vaccinesData={vaccinesData}
           onHandleSearch={handleSearch}
-        />
+        /> */}
       </div>
       <DefaultDataTable
         columns={columns({
@@ -95,7 +321,7 @@ function VaccinesClient({ refreshTrigger }: { refreshTrigger: number }) {
               type: "success",
               message: "Deleted Successfully",
             }),
-            fetchInjectionsData: () => fetchInjectionsData(),
+          fetchInjectionsData: () => fetchInjectionsData(),
         })}
         data={vaccinesData}
         pageNo={pageNo}
