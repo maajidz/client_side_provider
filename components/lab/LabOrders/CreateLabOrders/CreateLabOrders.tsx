821e78c073b884d7aef0bf0d3198548edc327f2e
humma-irshad <hummairshad48@gmail.com>
2025-03-03 19:51:19 +0530

    Fix `LabOrders`

--- components/lab/LabOrders/CreateLabOrders/CreateLabOrders.tsx
+++ components/lab/LabOrders/CreateLabOrders/CreateLabOrders.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState, useEffect, useCallback } from "react";
+import formStyles from "@/components/formStyles.module.css";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -11,7 +12,6 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { createLabResultsSchema } from "@/schema/createLabResultsSchema";
 import { zodResolver } from "@hookform/resolvers/zod/dist/zod.js";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
@@ -25,40 +25,69 @@ import ViewOrdersDialog from "@/components/charts/Encounters/SOAP/Labs/ViewOrder
 import { fetchUserDataResponse } from "@/services/userServices";
 import { UserData } from "@/types/userInterface";
 import SubmitButton from "@/components/custom_buttons/buttons/SubmitButton";
+import { LabOrdersInterface, LabsDataResponse } from "@/types/chartsInterface";
+import { createLabOrdersSchema } from "@/schema/createLabOrderSchema";
+import { fetchProviderListDetails } from "@/services/registerServices";
+import { FetchProviderListInterface } from "@/types/providerDetailsInterface";
+import { getLabsData } from "@/services/chartsServices";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { createLabOrders } from "@/services/labOrdersService";
+import { showToast } from "@/utils/utils";
+import { useToast } from "@/hooks/use-toast";
 
 const CreateLabResults = () => {
-  const form = useForm<z.infer<typeof createLabResultsSchema>>({
-    resolver: zodResolver(createLabResultsSchema),
-    defaultValues: {
-      patient: "",
-      reviewer: "",
-      dateTime: "",
-      labId: "",
-      testIds: [],
-      testResults: [
-        {
-          name: "",
-          result: "",
-          unit: "",
-          referenceMin: undefined,
-          referenceMax: undefined,
-          interpretation: "",
-          comment: "",
-          groupComment: "",
-        },
-      ],
-      tags: "",
-    },
+  // Labs Data
+  const [labResponse, setLabResponse] = useState<LabsDataResponse>({
+    data: [],
+    total: 0,
   });
 
   const [patients, setPatients] = useState<UserData[]>([]);
   const [searchTerm, setSearchTerm] = useState("");
+  // Providers List
+  const [providerListData, setProviderListData] =
+    useState<FetchProviderListInterface>({
+      data: [],
+      total: 0,
+    });
   const [visibleSearchList, setVisibleSearchList] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(false);
+  const [loading, setLoading] = useState({
+    provider: false,
+    patients: false,
+    lab: false,
+  });
+
+  // Toast State
+  const { toast } = useToast();
+
   const router = useRouter();
 
+  const form = useForm<z.infer<typeof createLabOrdersSchema>>({
+    resolver: zodResolver(createLabOrdersSchema),
+    defaultValues: {
+      orderedBy: "",
+      date: "",
+      labs: [],
+      tests: [],
+      isSigned: false,
+    },
+  });
+
   const fetchPatientList = useCallback(async () => {
-    setLoading(true);
+    setLoading((prev) => ({ ...prev, patients: true }));
+
     try {
       const response = await fetchUserDataResponse({
         pageNo: 1,
@@ -72,9 +101,53 @@ const CreateLabResults = () => {
     } catch (e) {
       console.log("Error", e);
     } finally {
-      setLoading(false);
+      setLoading((prev) => ({ ...prev, patients: false }));
     }
   }, [searchTerm]);
+
+  const fetchProvidersList = useCallback(async () => {
+    setLoading((prev) => ({ ...prev, provider: true }));
+    try {
+      const data = await fetchProviderListDetails({ page: 1, limit: 10 });
+      if (data) {
+        setProviderListData(() => ({
+          data: data.data,
+          total: data.total,
+        }));
+      }
+    } catch (e) {
+      console.log("Error", e);
+    } finally {
+      setLoading((prev) => ({ ...prev, provider: false }));
+    }
+  }, []);
+
+  // Labs list
+  const fetchLabsData = useCallback(async () => {
+    setLoading((prev) => ({ ...prev, lab: true }));
+
+    try {
+      const data = await getLabsData({ page: 1, limit: 10 });
+      if (data) {
+        setLabResponse(() => ({
+          data: data.data,
+          total: data.total,
+        }));
+        if (labResponse.total < data.total) {
+          await fetchLabsData();
+        }
+      }
+    } catch (e) {
+      console.log("Error", e);
+    } finally {
+      setLoading((prev) => ({ ...prev, lab: false }));
+    }
+  }, [labResponse?.total]);
+
+  useEffect(() => {
+    fetchProvidersList();
+    fetchLabsData();
+  }, [fetchProvidersList, fetchLabsData]);
 
   useEffect(() => {
     fetchPatientList();
@@ -86,11 +159,39 @@ const CreateLabResults = () => {
       .includes(searchTerm.toLowerCase())
   );
 
-  const onSubmit = async (values: z.infer<typeof createLabResultsSchema>) => {
-    console.log(values);
+  const onSubmit = async (values: z.infer<typeof createLabOrdersSchema>) => {
+    const requestData: LabOrdersInterface = {
+      orderedBy: values.orderedBy,
+      date: values.date,
+      labs: values.labs,
+      tests: values.tests,
+      userDetailsId: values?.patient ?? "",
+      isSigned: false,
+    };
+
+    try {
+      const response = await createLabOrders({ requestData });
+      if (response) {
+        showToast({
+          toast,
+          type: "success",
+          message: "Lab Result saved successfully!",
+        });
+        router.replace(`/dashboard/provider/labs`);
+      }
+    } catch (e) {
+      console.log("Error", e);
+      showToast({
+        toast,
+        type: "error",
+        message: "Error while saving Lab Results",
+      });
+    } finally {
+      form.reset();
+    }
   };
 
-  if (loading) {
+  if (loading.patients || loading.provider || loading.lab) {
     return <LoadingButton />;
   }
 
@@ -117,12 +218,12 @@ const CreateLabResults = () => {
             onSubmit={form.handleSubmit(onSubmit)}
             className="flex flex-col gap-4"
           >
-            <div className="flex gap-7">
+            <div className={`${formStyles.formBody} w-[30rem]`}>
               <FormField
                 control={form.control}
                 name="patient"
                 render={({ field }) => (
-                  <FormItem>
+                  <FormItem className={formStyles.formItem}>
                     <FormLabel>Patient</FormLabel>
                     <FormControl>
                       <div className="relative">
@@ -167,10 +268,47 @@ const CreateLabResults = () => {
               />
               <FormField
                 control={form.control}
-                name="dateTime"
+                name="orderedBy"
                 render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Ordered Data</FormLabel>
+                  <FormItem className={formStyles.formItem}>
+                    <FormLabel>Ordered By:</FormLabel>
+                    <FormControl>
+                      <div className="flex flex-col gap-2 items-start justify-start ">
+                        <Select
+                          value={field.value}
+                          onValueChange={(value: string) => {
+                            field.onChange(value);
+                          }}
+                        >
+                          <SelectTrigger>
+                            <SelectValue placeholder="Select" />
+                          </SelectTrigger>
+                          <SelectContent>
+                            {providerListData.data.map((providerList) => {
+                              const providerId =
+                                providerList.providerDetails?.id ??
+                                providerList.id;
+                              return (
+                                <SelectItem
+                                  key={providerList.id}
+                                  value={providerId}
+                                >{`${providerList.firstName} ${providerList.lastName}`}</SelectItem>
+                              );
+                            })}
+                          </SelectContent>
+                        </Select>
+                      </div>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="date"
+                render={({ field }) => (
+                  <FormItem className={formStyles.formItem}>
+                    <FormLabel>Ordered Date</FormLabel>
                     <FormControl>
                       <Input
                         type="date"
@@ -178,6 +316,115 @@ const CreateLabResults = () => {
                         placeholder="Select date"
                         className="w-fit"
                       />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="labs"
+                render={({ field }) => (
+                  <FormItem className={formStyles.formItem}>
+                    <FormLabel>Labs</FormLabel>
+                    <FormControl>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <button
+                            className="border px-4 py-2 rounded-md w-full text-left"
+                            type="button"
+                          >
+                            {field.value.length > 0
+                              ? field.value
+                                  .map(
+                                    (labId) =>
+                                      labResponse?.data?.find(
+                                        (lab) => lab.id === labId
+                                      )?.name
+                                  )
+                                  .join(", ")
+                              : "Select labs"}
+                          </button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-72 bg-white border rounded-md p-2">
+                          {labResponse?.data?.map((lab) => (
+                            <div
+                              key={lab.id}
+                              className="flex items-center space-x-2 cursor-pointer p-2 hover:bg-gray-100 rounded"
+                              onClick={() => {
+                                // Toggle lab selection
+                                const updatedLabs = field.value.includes(lab.id)
+                                  ? field.value.filter((id) => id !== lab.id)
+                                  : [...field.value, lab.id];
+
+                                form.setValue("labs", updatedLabs);
+                              }}
+                            >
+                              <Checkbox
+                                checked={field.value.includes(lab.id)}
+                              />
+                              <span>{lab.name}</span>
+                            </div>
+                          ))}
+                        </PopoverContent>
+                      </Popover>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="tests"
+                render={({ field }) => (
+                  <FormItem className={formStyles.formItem}>
+                    <FormLabel>Tests</FormLabel>
+                    <FormControl>
+                      <Popover>
+                        <PopoverTrigger asChild>
+                          <button
+                            className="border px-4 py-2 rounded-md w-full text-left"
+                            type="button"
+                          >
+                            {field.value.length > 0
+                              ? field.value
+                                  .map(
+                                    (testId) =>
+                                      labResponse?.data
+                                        ?.flatMap((lab) => lab.tests)
+                                        ?.find((test) => test.id === testId)
+                                        ?.name
+                                  )
+                                  .join(", ")
+                              : "Select tests"}
+                          </button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-72 bg-white border rounded-md p-2">
+                          {labResponse?.data?.flatMap((lab) =>
+                            lab.tests.map((test) => (
+                              <div
+                                key={test.id}
+                                className="flex items-center space-x-2 cursor-pointer p-2 hover:bg-gray-100 rounded"
+                                onClick={() => {
+                                  // Toggle selection
+                                  const updatedTests = field.value.includes(
+                                    test.id
+                                  )
+                                    ? field.value.filter((id) => id !== test.id)
+                                    : [...field.value, test.id];
+
+                                  form.setValue("tests", updatedTests);
+                                }}
+                              >
+                                <Checkbox
+                                  checked={field.value.includes(test.id)}
+                                />
+                                <span>{test.name}</span>
+                              </div>
+                            ))
+                          )}
+                        </PopoverContent>
+                      </Popover>
                     </FormControl>
                     <FormMessage />
                   </FormItem>
