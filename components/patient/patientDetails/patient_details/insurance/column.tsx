4492d760d1a9736601611e8574c338b5dc80c616
maajidz <maajidz@yahoo.com>
2025-03-06 15:26:52 +0530

    add Ellipsis

--- components/patient/patientDetails/patient_details/insurance/column.tsx
+++ components/patient/patientDetails/patient_details/insurance/column.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { Ellipsis } from "lucide-react";
+import { Ellipsis, EllipsisVertical } from "lucide-react";
 import {
   InsuranceResponse,
   UpdateInsuranceType,
